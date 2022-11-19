900fed3b91b9e803d7d2c7323de88925410121df
LTDigor <ovd2001@mail.ru>
2022-11-19 13:06:51 +0300

    done lr1

---
+++ lab1/DecisionTrees.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+SEX_TO_NUM_DICT = {
+    "male": 1,
+    "female": 0
+}
+
+
+def main():
+    # load data
+    data = pd.read_csv("titanic.csv")
+
+    # clear data from NaN
+    data = data.dropna().reset_index(drop=True)
+
+    # select the desired columns
+    df = data[['Pclass', 'Fare', 'Age', 'Sex']]
+
+    # replace sex with number
+    df['Sex'] = df['Sex'].apply(lambda x: SEX_TO_NUM_DICT[x])
+
+    # get target variable
+    survived = data['Survived']
+
+    # training
+    clf = DecisionTreeClassifier(random_state=241)
+    clf.fit(df, survived)
+
+    # get the 2 most important features
+    importances = clf.feature_importances_
+    features_importances = list(zip(importances, df.columns.values.tolist()))
+    features_importances.sort(key=lambda x: x[0], reverse=True)
+
+    # show result
+    print(features_importances[0][1], features_importances[1][1], sep=',')
+
+
+if __name__ == "__main__":
+    main()
